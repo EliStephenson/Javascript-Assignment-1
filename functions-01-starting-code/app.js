081494e3c2d69a5506a0b9c4c1ecd16aede15324
EliStephenson <elistephenson17@gmail.com>
2023-02-27 19:37:55 -0700

    added arrow functions to simplify code

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -10,52 +10,49 @@ const RESULT_COMPUTER_WIN = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function() {
-    const selection = prompt(`${ROCK},${PAPER} or ${SCISSORS}?`,'').toUpperCase();
-    if (selection !== ROCK &&
-        selection !== PAPER &&
-        selection !== SCISSORS
-    ) {
-        alert(`Invalid choice! We chose ${Rock} for you!`);
-        return DEFAULT_USER_CHOICE
-    }
-    return selection;
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK},${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${Rock} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 };
 
-const getComputerChoice = function() {
-    const randomValue = Math.random();
-    if (randomValue < 0.34) {
-        return ROCK;
-    } else if (randomValue < 0.67) { // wont kick in because its checkde after and there is a return
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    // wont kick in because its checkde after and there is a return
     return PAPER;
-    } else { 
+  } else {
     return SCISSORS;
-}
-} ; 
-
-const getWinner = function(cChoice, pChoice) {
-    if (cChoice === pChoice) {
-        return RESULT_DRAW;
-    } else if (cChoice === ROCK && pChoice === PAPER || 
-               cChoice === PAPER && pChoice === SCISSORS ||
-               cChoice === SCISSORS && pChoice === ROCK ) 
-    {
-        return RESULT_PLAYER_WIN;
-    } else {
-        return RESULT_COMPUTER_WIN;
-    }
+  }
 };
 
+
+const getWinner = (cChoice, pChoice) => 
+    cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WIN
+    : RESULT_COMPUTER_WIN;
 
 
 startGameBtn.addEventListener('click', function startGame() {
-    if (gameIsRunning) {
-        return;
-    }
-    gameIsRunning = true;
-    console.log('Game is starting...');
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-    const winner = getWinner(computerChoice, playerChoice)
-    console.log(winner)
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 }); // name just used for debugging (give anonymous functions a name)
